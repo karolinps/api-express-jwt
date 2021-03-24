5126b03b630f38c29410d471b171b59703cc6c0a
Karolin APS <porrask92@gmail.com>
2021-03-24 17:56:04 -0500

    user register / validate campos and email unique

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,46 @@
+const Joi = require("@hapi/joi");
+const User = require("../models/user");
 
+const schemaRegister = Joi.object({
+  name: Joi.string().min(6).max(255).required(),
+  email: Joi.string().min(6).max(255).required().email(),
+  password: Joi.string().min(6).max(255).required(),
+});
 
 const singUp = async (req, res) => {
-  res.json({
-    message: "Aqui va el mensaje",
-    data: "",
-  });
+  //validamos campos users
+  const { error } = schemaRegister.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({
+      message: error.details[0].message,
+    });
+  }
+
+  //capturamos los values desde el body
+  const { name, email, password } = req.body;
+
+  const emailExist = await User.findOne({ email: email });
+  //Validamos si ya existe el correo en la db
+  if (emailExist) {
+    return res.status(400).json({
+      message: "Este email ya está registrado",
+    });
+  }
+
+  //Creamos un new object user
+  const user = new User({ name, email, password });
+
+  try {
+    const savedUser = await user.save();
+
+    res.status(200).json({
+      message: "Se ha registrado exitosamente",
+      data: savedUser,
+    });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("./app");
 require("./database");
 require('dotenv').config();
 
-const port = process.env.PORT ;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server run in port ${port}`);
 });
