8d477636c0e066918965ac4b56203281a539a25a
Karolin APS <porrask92@gmail.com>
2021-03-24 16:21:46 -0500

    app listen port

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const app = express();
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server run in port ${PORT}`);
+});
